f8fd1bf708ee2564473149063ab950fbc082d3dd
Assem Bakr <assem.bakr00@gmail.com>
2026-09-26 17:06:22 +0300

    Refactor player animations and improve tilemap integration
    - Updated player scene to include new run animation using AtlasTextures.
    - Adjusted player movement logic to differentiate between running and moving.
    - Refactored animation handling in player script for better clarity and efficiency.
    - Integrated new dirt and root tilemaps into level 1 scene.
    - Cleaned up unused resources and optimized scene structure.
    - Changed editor settings for improved usability and adjusted recent files order.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -15,53 +15,43 @@ var vertical_scale = 7
 var stopping_speed = 0.1
 
 func _physics_process(delta: float) -> void:
-	
-	# ALlow the player to move around
 	move(delta)
-	
-	# Poitn the player in the correcct direction
-	$AnimatedSprite2D.flip_h = velocity.x < 0
-	
-	var animation_type = get_animation_type()
-	
-	if animation_type == "move" or animation_type == "jump" or animation_type == "fall":
-		$AnimatedSprite2D.play(animation_type)
-	
 	move_and_slide()
+	_update_animation()
 
-func get_animation_type():
-	#print("velocity is ", velocity)
-	
-	# Check if the player is stopped
-	if abs(velocity.length()) < stopping_speed:
-		return "idle"
-	
-	# Check if the player is falling
-	if velocity.y > stopping_speed:
-		return "fall"
-	
-	# Check if the player is jumping up
-	if velocity.y < -stopping_speed:
+
+func _update_animation() -> void:
+	var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+	animated_sprite.flip_h = velocity.x < 0
+	var animation_type: String = get_animation_type()
+
+	if animated_sprite.animation != animation_type:
+		animated_sprite.play(animation_type)
+
+func get_animation_type() -> String:
+	if not is_on_floor():
+		if velocity.y > stopping_speed:
+			return "fall"
 		return "jump"
-	
-	# Check if the player is moving to the left or right
+
 	if abs(velocity.x) > stopping_speed:
-		return "move"
+		return "run"
+
+	return "move"
+
 
 func move(delta):
 	target_vel = Vector2.ZERO
-	
-	# Add vertical movement
+
 	if is_on_floor():
 		if Input.is_action_pressed("move_up"):
 			velocity.y = JUMP_VELOCITY * vertical_scale
 	else:
-		# Add gravity
 		velocity += get_gravity() * delta * vertical_scale
-	
+
 	if Input.is_action_pressed("move_left"):
 		target_vel[0] += -SPEED
 	if Input.is_action_pressed("move_right"):
 		target_vel[0] += SPEED
-	
+
 	velocity += (target_vel - velocity) / player_acceleration
